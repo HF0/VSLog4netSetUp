4d70df93bc0a24f389ae0f0cb8fd60f47c19bb4c
Jamie <HF0@users.noreply.github.com>
2015-10-13 15:30:24 +0200

    First source commit

---
+++ DllThatUsesLog4netTest/DumbClass.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllThatUsesLog4netTest
+{
+    public class DumbClass
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(DumbClass));
+
+        public DumbClass()
+        {
+
+
+        }
+
+        public void dumbOperation()
+        {
+            log.InfoFormat("This is a message from a dll");
+        }
+
+    }
+}
---
+++ Mainlog4nettest/Program.cs
@@ -0,0 +1,25 @@
+﻿using DllThatUsesLog4netTest;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mainlog4nettest
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            log.InfoFormat("Logger started");
+
+            DumbClass dumbInstance = new DumbClass();
+            dumbInstance.dumbOperation();
+
+            Console.ReadKey();
+        }
+    }
+}
